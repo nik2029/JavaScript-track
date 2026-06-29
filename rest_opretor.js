9acf3fc95841b5735233e038e582257c1ca08ce6
Abhibykat Singh <nik7273singh@gmail.com>
2026-06-29 13:59:37 +0530

    learn rest opretor

---
+++ rest_opretor.js
@@ -0,0 +1,15 @@
+const arr=[55,78,58,5,85,8,45];
+const [first,,third]=arr;
+console.log(first);
+console.log(third);
+
+
+///
+
+const arr1=[5,45,85,85,58];
+const[first,...remaining]=arr1;
+console.log(first);
+console.log(remaining);
+
+
+
